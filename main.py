27ad6fbd28fe2b5d7265d7409d30cab4bb7de943
codewithshadyy <kipkoechshadrack10@gmail.com>
2025-12-03 07:39:06 +0000

    added  control flow to our budget project

--- main.py
+++ main.py
@@ -9,21 +9,81 @@
 
 
 """
-
+# step 2
 print(" ====Monthly Budget==== ")
 
 name = input("enter your name:")
-monthly_income = float(input("Enter yur monthly income:$",))
-rent = float(input("enter your rent Expenses:$"))
-groceries = float(input("enter your groceries Expenses:$"))
-transport = float(input("enter your transport Expenses:$"))
-entertainment = float(input("enter your entertaiment Expenses:$"))
 
+valid = False
+
+while not valid:
+    
+    monthly_income = input("Enter yur monthly income:$",)
+    
+    if monthly_income.replace(".", "", 1).isdigit():
+        monthly_income = float(monthly_income)
+        valid = True
+    
+    else:
+        print("enter a monthly income")    
+          
+    
+    
+valid = False
+
+while not valid:    
+    
+    rent = input("enter your rent Expenses:$")
+    if rent.replace(".", "", 1).isdigit():
+        rent = float(rent)
+        valid = True
+    else:
+        print("enter a valid rent")    
+             
+    
+valid = False 
+while not valid:  
+    groceries = input("enter your groceries Expenses:$")
+    
+    if groceries.replace(".", "", 1).isdigit():
+        groceries = float(groceries)
+        valid = True
+    else:
+        print("enter a valid groceries expenses")
+        
+valid = False 
+while not valid:  
+    transport = input("enter your transport Expenses:$")
+    
+    if transport.replace(".", "", 1).isdigit():
+        transport = float(transport)
+        valid = True
+    else:
+        print("enter a valid transport expenses")
+        
+
+valid = False 
+while not valid:  
+    entertainment = input("enter your entertainment Expenses:$")
+    
+    if entertainment.replace(".", "", 1).isdigit():
+        entertainment = float(entertainment)
+        valid = True
+    else:
+        print("enter a valid entertainment expenses")
+
+# step 3
 total_expenses = rent + groceries + transport + entertainment
-savings = monthly_income - total_expenses
-savings_ratio = (savings / monthly_income) * 100
+remaining_balance = monthly_income - total_expenses
+# savings = monthly_income - total_expenses
+if monthly_income > 0:
+    savings_ratio = (remaining_balance / monthly_income) * 100
+else:
+    savings_ratio = 0
 
 
+
+# step 4
 print("==== Monthly budget Summary==== ")
 print(f"Monthly budget summmary for {name}")
 print(f"monthly income:${monthly_income}")
@@ -33,3 +93,36 @@ print(f"Groceries Expenses:${groceries}")
 print(f"Transport expenses:${transport}")
 print(f"Entertainment Expenses:${entertainment}")
 print(f"Savings Ratio:${savings_ratio}")
+
+# step 5
+
+print("==============================")
+
+if savings_ratio < 10:
+    print("YOur savings are low, try reducing expenses")
+elif savings_ratio < 30:
+    print("You are saving fairly, keep improving")
+else:
+    print("great job!!, you are saving nicely")   
+    
+ 
+choice =  input("\nWould you like to see breakdown of your expenses?(yes/no):").strip().lower()
+
+if choice == "yes":
+    categories = ["Rent", "Groceries", "Transport", "Entertainment"]
+    values = [rent, groceries, transport, entertainment]
+    total = total_expenses
+    
+    print("\nExpense breakdown")
+    
+    i = 0
+    while i < len(categories):
+        percent = (values[i] / total) * 100
+        print(categories[i], ":",  f"${values[i]:.2f}", ":" f"({percent:.2f}%)")
+        
+        i += 1
+        
+        
+print("===Thank You for using the Personal Budget aving tool")        
+     
+             
